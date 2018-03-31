e6e79d86f6474bde4f88e53774d2a2e6b1fc385a
Andre Kazmierczak <ak@cobago.de>
2018-03-31 20:17:21 +0200

    Initial Commit

---
+++ Stimpak/Stimpak.h
@@ -0,0 +1,19 @@
+//
+//  Stimpak.h
+//  Stimpak
+//
+//  Created by Andre Kazmierczak on 31.03.18.
+//  Copyright © 2018 Andre Kazmierczak. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Stimpak.
+FOUNDATION_EXPORT double StimpakVersionNumber;
+
+//! Project version string for Stimpak.
+FOUNDATION_EXPORT const unsigned char StimpakVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stimpak/PublicHeader.h>
+
+
